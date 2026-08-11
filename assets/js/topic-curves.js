a2aef29c8d07580201a71c193b86bf0c517f2eaf
Rebecca-Owens <beccaowensncl@gmail.com>
2026-08-11 21:34:06 +0100

    Change minimum width for drawing curves to 640px

--- assets/js/topic-curves.js
+++ assets/js/topic-curves.js
@@ -6,7 +6,7 @@
   'use strict';
 
   var SVG_NS = 'http://www.w3.org/2000/svg';
-  var MIN_WIDTH = 900;          // below this, curves are not drawn
+  var MIN_WIDTH = 640;          // below this, curves are not drawn
   var CONTROL_RATIO = 0.5;      // horizontal pull of the bezier handles
   var LAND_SPREAD = 26;         // px spread of landing points on a multi-topic paper
 
